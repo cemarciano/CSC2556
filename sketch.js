78c55eb0a9938ac1b454dded781d25f22e6be873
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2021-03-28 00:58:55 -0300

    Create interface

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-// data[i] is the location of data point *i*
+// data[i] is the x-coordinate of data point *i*
 var data = [];
-// mean[i] is the location of cluster *i*
-var mean = [];
-// c[i] is the number of the cluster assigned to data point *i*
-var c = [];
+// clusters[i] is the x-coordinate location of cluster *i*
+var clusters = [];
+// data2cluster[i] is the number of the cluster assigned to data point *i*
+var data2cluster = [];
 
-var isdata, ismean, islearning, step;
+var insertingData, insertingCentroids, islearning, step;
 
 var colors = ["blue", "red", "yellow", "green", "pink", "white"];
 var colorc = 0;
@@ -13,17 +13,38 @@ var colorc = 0;
 var instruction = "Tap on the Screen to Insert Data Points...   Press Enter to Insert Cluster Centroids";
 
 var button;
+var sel;
+var mu_input;
+var mu = "0";
+var sigma_input;
+var sigma = "1";
+var numContents_input;
+var numContents = "15";
+
+var manualMode = false;
 
 function setup () {
-    isdata = true;
-    ismean = false;
+    insertingData = true;
+    insertingCentroids = false;
     islearning = false;
     step = false;
+    numContents_input = createInput(numContents);
+    numContents_input.input(updateNumContents);
     button = createButton('Generate random points');
+    button.mouseClicked(generateData);
+    sel = createSelect();
+    sel.option('Normal Distribution');
+    sel.selected('Normal Distribution');
+    mu_input = createInput(mu);
+    mu_input.input(updateMu);
+    sigma_input = createInput(sigma);
+    sigma_input.input(updateSigma);
+    //sel.changed(undefined);
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw () {
+
 
 
     let offsetX = windowWidth - 130
@@ -31,7 +52,13 @@ function draw () {
     let arrowSlope = 40
 
 
-    button.position(windowWidth/2 - 100, offsetY*0.5);
+    if ((mouseX > windowWidth - offsetX) && (mouseX < offsetX) && (mouseY < offsetY + 70) && (mouseY > offsetY - 70)) {
+        manualMode = true;
+    } else {
+        manualMode = false;
+    }
+
+
 
 
     background(51);
@@ -52,6 +79,41 @@ function draw () {
     textSize(35);
     text("\u211D", offsetX - 20, offsetY + 80);
 
+    // Instructions rectangle:
+    rect(windowWidth/2 - 350, 100, 700, 350, 20);
+    fill(30);
+    textSize(24);
+    text("OR", windowWidth/2 - 12, 280);
+    textSize(20);
+    text("Generate content based\non a distribution:", windowWidth/2 - 320, 140);
+    textAlign(CENTER);
+    text("Click on the real\nline below to add\ncontent manually\n(or both!)", windowWidth/2 + 175, 225);
+    noStroke();
+    textSize(17);
+    text("Number of contents:", windowWidth/2 - 230, 212);
+    numContents_input.size(60);
+    numContents_input.position(windowWidth/2 - 320, 225);
+    text("Distribution:", windowWidth/2 - 257, 275);
+    sel.position(windowWidth/2 - 320, 287);
+    text("Parameters:", windowWidth/2 - 265, 335);
+    // Mu (mean):
+    text("\u03BC:", windowWidth/2 - 307, 360);
+    mu_input.size(30);
+    mu_input.position(windowWidth/2 - 290, 345);
+    // Sigma squared (variance):
+    text("\u03C3\u00B2:", windowWidth/2 - 220, 360);
+    sigma_input.size(30);
+    sigma_input.position(windowWidth/2 - 200, 345);
+    // Button to generate:
+    button.position(windowWidth/2 - 250, 400);
+    // "OR" separation:
+    stroke(0);
+    strokeWeight(1.5);
+    line(windowWidth/2, 117, windowWidth/2, 255);
+    line(windowWidth/2, 290, windowWidth/2, 435);
+    noStroke();
+    textAlign(LEFT);
+
     // Real line:
     stroke(255);
     strokeWeight(2);
@@ -64,7 +126,7 @@ function draw () {
     line(offsetX, offsetY, offsetX - arrowSlope, offsetY + 0.7*arrowSlope);
 
     // Dashed line:
-    if ((mouseX > windowWidth - offsetX) && (mouseX < offsetX)) {
+    if (manualMode == true) {
         drawingContext.setLineDash([5, 15]);
         line(mouseX, 0, mouseX, windowHeight);
         drawingContext.setLineDash([]);
@@ -78,48 +140,52 @@ function draw () {
     // Draws data points:
     for (var i = 0; i < data.length; i++) {
         noStroke();
-        fill(color(colors[c[i]]));
-        ellipse(data[i], offsetY, 10, 10);
+        fill(color(colors[data2cluster[i]]));
+        ellipse(data[i], offsetY, 9, 9);
     }
     // Draws centroids:
-    for (var i = 0; i < mean.length; i++) {
+    for (var i = 0; i < clusters.length; i++) {
         noStroke();
         fill(color(colors[i]));
-        ellipse(mean[i], offsetY, 20, 20);
+        ellipse(clusters[i], offsetY, 20, 20);
     }
     noFill();
 }
 
 function mouseClicked () {
-    if (isdata) {
-        data.push(mouseX);
-        c.push(5);
-    } else if (ismean) {
-        mean.push(mouseX);
-        colorc++;
+    if (manualMode == true) {
+        // If user is currently inserting data points:
+        if (insertingData) {
+            data.push(mouseX);
+            data2cluster.push(5);
+        // If user is currently inserting clusters:
+        } else if (insertingCentroids) {
+            clusters.push(mouseX);
+            colorc++;
+        }
     }
 }
 
 function keyPressed () {
     if (keyCode === ENTER) {
-        if (isdata == true && ismean == false) {
-            isdata = false;
-            ismean = true;
+        if (insertingData == true && insertingCentroids == false) {
+            insertingData = false;
+            insertingCentroids = true;
             instruction = "Tap on the Screen to Insert Cluster Centroids...   Press Enter to Start Clustering";
-        } else if (isdata == false && ismean == true) {
-            ismean = false;
+        } else if (insertingData == false && insertingCentroids == true) {
+            insertingCentroids = false;
             islearning = true;
             instruction = "Press Enter to Assign the Clusters to Data Points...";
-        } else if (isdata == false && ismean == false && islearning == true) {
+        } else if (insertingData == false && insertingCentroids == false && islearning == true) {
             if (step) {
-                for (var i = 0; i < mean.length; i++) {
-                    mean[i] = average(i);
+                for (var i = 0; i < clusters.length; i++) {
+                    clusters[i] = findClusterPos(i);
                 }
                 instruction = "Press Enter to Assign the Clusters to Data Points...";
             } else {
                 for (var i = 0; i < data.length; i++) {
                     // Finds the closest centroid to data point *i*:
-                    c[i] = closestCentroid(i);
+                    data2cluster[i] = closestCentroid(i);
                 }
                 instruction = "Press Enter to Update the Cluster Centroids...";
             }
@@ -129,43 +195,58 @@ function keyPressed () {
 }
 
 // Calculates position of cluster *n*:
-function average (n) {
-
+function findClusterPos(cluster) {
     var sumX = 0, count = 0;
     var avgX;
-
     // For each data point:
     for (var i = 0; i < data.length; i++) {
         // If data point *i* is assigned to cluster *n*:
-        if (c[i] == n) {
+        if (data2cluster[i] == cluster) {
             sumX += data[i];
             count++;
         }
     }
-
     avgX = Math.trunc(sumX / count);
-
     return avgX;
 }
 
-// Returns the closest centroid to data point *n*:
-function closestCentroid (n) {
-
+// Returns the closest centroid to data point *x*:
+function closestCentroid (x) {
     var minIndex = 0;
-    var minDistance = distanceLine(data[n], mean[minIndex]);
-
-    for (var i = 1; i < mean.length; i++) {
-        var dis = distanceLine(data[n], mean[i]);
-
+    var minDistance = distanceLine(data[x], clusters[minIndex]);
+    for (var i = 1; i < clusters.length; i++) {
+        var dis = distanceLine(data[x], clusters[i]);
         if (dis < minDistance) {
         minDistance = dis;
         minIndex = i;
         }
     }
-
     return minIndex;
 }
 
 function distanceLine (x1, x2) {
     return Math.abs(x1 - x2);
+}
+
+function generateData(){
+    console.log("Called")
+    gaussian_mean = windowWidth/2 + parseInt(mu)*(windowWidth/20);
+    gaussian_stddev = parseInt(sigma)*(windowWidth/20);
+    for (i = 0; i < parseInt(numContents); i++) {
+        data.push(randomGaussian(gaussian_mean, gaussian_stddev));
+        data2cluster.push(5);
+    }
+}
+
+
+function updateNumContents(){
+    numContents = this.value();
+}
+
+function updateMu(){
+    mu = this.value();
+}
+
+function updateSigma(){
+    sigma = this.value();
 }
